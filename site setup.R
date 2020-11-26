def987ff8cab5f4c0d01e6352094e6cd70724e31
greg-dubrow <dubrowg@gmail.com>
2020-11-26 15:24:02 -0800

    inital commit

--- site setup.R
+++ site setup.R
@@ -1,5 +1,10 @@
-blogdown::new_site(
-	dir = "Theme",
-	install_hugo = FALSE,
-	theme = "gcushen/hugo-academic",
-	theme_example = TRUE, empty_dirs = TRUE)
+# blogdown::new_site(
+# 	dir = "Theme",
+# 	install_hugo = FALSE,
+# 	theme = "gcushen/hugo-academic", theme_example = TRUE,
+# 	sample = TRUE, to_yaml = TRUE,
+# 	empty_dirs = TRUE)
+#
+# blogdown::build_site()
+
+blogdown::serve_site()
