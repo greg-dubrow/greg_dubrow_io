2b6db18b79648c84e2de3e2a1052dc726ca2935f
greg-dubrow <dubrowg@gmail.com>
2020-11-26 14:19:54 -0800

    redo

--- site setup.R
+++ site setup.R
@@ -1,4 +1,5 @@
 blogdown::new_site(
 	dir = "Theme",
 	install_hugo = FALSE,
-	theme = "gcushen/hugo-academic", theme_example = TRUE)
+	theme = "gcushen/hugo-academic",
+	theme_example = TRUE, empty_dirs = TRUE)
